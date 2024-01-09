56bdf38a52a04e1fc8d0d5c84ec87f5cbe139805
RosenUrkov <rosen.urkov@gmail.com>
2024-01-09 13:52:25 +0200

    Added vite rollup polyfill

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import eslintPlugin from 'vite-plugin-eslint'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslintPlugin from 'vite-plugin-eslint';
+import polyfillNode from 'rollup-plugin-polyfill-node';
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      plugins: [polyfillNode()],
+    },
+  },
   plugins: [
     react(),
     eslintPlugin({
@@ -11,4 +17,4 @@ export default defineConfig({
       exclude: [],
     }),
   ],
-})
+});
