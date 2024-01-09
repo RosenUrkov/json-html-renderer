e7b10452b23dafa80011c1c8542346e0b22198bd
RosenUrkov <rosen.urkov@gmail.com>
2024-01-09 16:00:55 +0200

    Revert "Added vercel vite plugin"
    This reverts commit 470f25116cac7c8223731c0e8d46a344f0ba2664.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslintPlugin from 'vite-plugin-eslint';
-import vercel from 'vite-plugin-vercel';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
   plugins: [
     react(),
-    vercel(),
     eslintPlugin({
       cache: false,
       include: ['./src/**/*.ts', './src/**/*.tsx'],
